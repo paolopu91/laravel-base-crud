b0ae0ae7f82a63d1c07454732332b01b742b9101
paolopu91 <paolo.pugliese1991@gmail.com>
2022-08-02 19:02:00 +0200

    adds changes

--- resources/views/data/show.blade.php
+++ resources/views/data/show.blade.php
@@ -3,9 +3,17 @@
 @section('page_title', "Description #" . $data->id)
     
 @section('page_content')
+<div>
+    <div class="container">
+        <h2 class="pt-5 pb-3">{{ $data->title }}</h2>
+        <img class="px-5 py-5" src="{{$data['thumb']}}" alt="">
+    </div>
+    
+    <div class="container">
+        <p class="pt-3">{{ $data['description'] }}</p>
+    </div>
+</div>
 
-    <h2>{{ $data->title }}</h2>
-    <img src="{{$data['thumb']}}" alt="">
-    <p>{{ $data['description'] }}</p>
+    
 
 @endsection
